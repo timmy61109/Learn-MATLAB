590864ad6a697b43510ff6ec937b7c32139caca2
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-12-26 12:56:12 +0800

    此提交將完成作業 14 9 例題
    修改項目:
    
    模組:
    
    議題

--- src/ex14_9.m
+++ src/ex14_9.m
@@ -1,5 +1,9 @@
-function out = ex14_9()
+function [t, y] = ex14_9()
 %  Description
-%   = ()
+%  [t, y] = ex14_9()
 %
 % Long description
+
+dy = @(t, y) sin(t) - y * cos(t);
+ode45(dy, [0, 2], 6);
+[t, y] = ode45(dy, [0, 2], 6);
