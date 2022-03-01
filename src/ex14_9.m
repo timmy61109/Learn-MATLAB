82aa71bbdc8a5aa470ed63bd3d9a29781d567c4b
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-03-01 15:45:08 +0800

    此提交將修正運算方式
    修改項目:
    
    模組:
    
    議題

--- src/ex14_9.m
+++ src/ex14_9.m
@@ -4,6 +4,6 @@ function [t, y] = ex14_9()
 %
 % Long description
 
-dy = @(t, y) sin(t) - y * cos(t);
-ode45(dy, [0, 2], 6);
-[t, y] = ode45(dy, [0, 2], 6);
+% dy = @(t, y) sin(t) - y * cos(t);
+dsolve('sin(t) - y * cos(t)', 'y(0)=6');
+ezplot(ans, [0, 2]);
